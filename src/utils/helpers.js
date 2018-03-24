0ec736f48e3c68b25b0745f71420dd2edfaec985
Amirali Esmaeili <esmailiamirali@gmail.com>
2018-03-25 00:20:35 +0430

    Add jsdoc for functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
+/**
+ * Generates a http error with status code and error message
+ * @param {number} status Http status code
+ * @param {string} message Error message
+ * @returns {object} Error object
+ */
 function genHttpError(status, message) {
   const err = new Error(message);
   err.status = status;
--- src/utils/tokenGenerator.js
+++ src/utils/tokenGenerator.js
@@ -6,12 +6,23 @@ import config from '../config';
 const randomBytes = promisify(crypto.randomBytes);
 const jwtSign = promisify(jwt.sign);
 
+/**
+ * Generates a cryptographic random hash as refresh token
+ * @async
+ * @returns {Promise<string>} Promise object resolved with refresh token
+ */
 async function genRefreshToken() {
   const buffer = await randomBytes(256);
   const refreshToken = crypto.createHash('sha1').update(buffer).digest('hex');
   return refreshToken;
 }
 
+/**
+ * Generates a jwt access token for particular userId
+ * @async
+ * @param {string} userId user id to be signed
+ * @returns {Promise<string>} Promise object resolved with access token
+ */
 async function genAccessToken(userId) {
   const payload = {
     sub: userId,
