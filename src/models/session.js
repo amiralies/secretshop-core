03333b22283c552d95987e610987566dbdb499a4
Amirali Esmaeili <esmailiamirali@gmail.com>
2018-03-28 05:01:46 +0430

    Update session model

--- src/models/session.js
+++ src/models/session.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const sessionSchema = new mongoose.Schema({
   userId: { type: mongoose.SchemaTypes.ObjectId, ref: 'User', required: true },
-  refreshToken: { type: 'String', required: true },
+  refreshToken: { type: String, required: true, unique: true },
+  dateCreated: { type: Date, default: Date.now(), required: true },
 });
 
 export default mongoose.model('Session', sessionSchema);
