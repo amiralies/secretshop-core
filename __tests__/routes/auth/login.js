ecbb03f93e6402fc1fc538046932ec0dcc435b22
Amirali Esmaeili <esmailiamirali@gmail.com>
2018-03-31 17:35:56 +0430

    Add refreshToken grantType to login route

--- __tests__/routes/auth/login.js
+++ __tests__/routes/auth/login.js
@@ -7,14 +7,39 @@ const Session = mongoose.model('Session');
 const server = request.agent(app);
 
 const mockUser = {
+  grantType: 'password',
   email: 'testUser@live.com',
   password: 'testPassword',
+};
+
+const mockUserWithRefreshToken = {
+  grantType: 'refreshToken',
+  refreshToken: '',
 };
 
 describe('/auth/register route', () => {
   beforeAll(async () => {
     await server.post('/auth/register')
-      .send({ ...mockUser, name: 'Steve jones' });
+      .send({ name: 'Steve jones', email: mockUser.email, password: mockUser.password });
+    const { body } = await server.post('/auth/login')
+      .send(mockUser);
+    mockUserWithRefreshToken.refreshToken = body.refreshToken;
+  });
+
+  it('should fail on login a user with no grantType', async () => {
+    const noGrantTypeUser = { email: mockUser.email, password: mockUser.password };
+    const { statusCode, body } = await server.post('/auth/login')
+      .send(noGrantTypeUser);
+    expect(statusCode).toBe(400);
+    expect(body.message).toBe('child "body" fails because [child "grantType" fails because ["grantType" is required]]');
+  });
+
+  it('should fail on login a user with invalid grantType', async () => {
+    const invalidGrantTypeUser = { ...mockUser, grantType: 'invalidGrantType' };
+    const { statusCode, body } = await server.post('/auth/login')
+      .send(invalidGrantTypeUser);
+    expect(statusCode).toBe(400);
+    expect(body.message).toBe('child "body" fails because [child "grantType" fails because ["grantType" must be one of [password, refreshToken]]]');
   });
 
   it('should fail on login a user with invalid email', async () => {
@@ -26,38 +51,38 @@ describe('/auth/register route', () => {
   });
 
   it('should fail on login a user with no email', async () => {
-    const invalidMailUser = { password: mockUser.password };
+    const noMailUser = { grantType: mockUser.grantType, password: mockUser.password };
     const { statusCode, body } = await server.post('/auth/login')
-      .send(invalidMailUser);
+      .send(noMailUser);
     expect(statusCode).toBe(400);
     expect(body.message).toBe('child "body" fails because [child "email" fails because ["email" is required]]');
   });
 
   it('should fail on login a user with no password', async () => {
-    const invalidPasswordUser = { email: mockUser.email };
+    const noPasswordUser = { grantType: mockUser.grantType, email: mockUser.email };
     const { statusCode, body } = await server.post('/auth/login')
-      .send(invalidPasswordUser);
+      .send(noPasswordUser);
     expect(statusCode).toBe(400);
     expect(body.message).toBe('child "body" fails because [child "password" fails because ["password" is required]]');
   });
 
   it('should fail on login with unregistered email', async () => {
-    const invalidMailUser = { ...mockUser, email: 'unregistered@thisisunregistered.io' };
+    const unregisteredMailUser = { ...mockUser, email: 'unregistered@thisisunregistered.io' };
     const { statusCode, body } = await server.post('/auth/login')
-      .send(invalidMailUser);
+      .send(unregisteredMailUser);
     expect(statusCode).toBe(400);
     expect(body.message).toBe('User not found');
   });
 
   it('should fail on login with wrong password', async () => {
-    const invalidMailUser = { ...mockUser, password: 'wrongPw' };
+    const wrongPasswordUser = { ...mockUser, password: 'wrongPw' };
     const { statusCode, body } = await server.post('/auth/login')
-      .send(invalidMailUser);
+      .send(wrongPasswordUser);
     expect(statusCode).toBe(400);
     expect(body.message).toBe('Wrong password');
   });
 
-  it('should login a user successfully', async () => {
+  it('should login a user with email and password successfully', async () => {
     const { statusCode, body } = await server.post('/auth/login').send(mockUser);
     expect(statusCode).toBe(201);
     expect(body.success).toBeTruthy();
@@ -68,9 +93,49 @@ describe('/auth/register route', () => {
     expect(body.accessToken).not.toBe('');
   });
 
+  it('should fail on obtain an accessToken with no grantType', async () => {
+    const noGrantTypeUser = { refreshToken: mockUserWithRefreshToken.refreshToken };
+    const { statusCode, body } = await server.post('/auth/login')
+      .send(noGrantTypeUser);
+    expect(statusCode).toBe(400);
+    expect(body.message).toBe('child "body" fails because [child "grantType" fails because ["grantType" is required]]');
+  });
+
+  it('should fail on obtain an accessToken with invalid grantType', async () => {
+    const invalidGrantTypeUser = { ...mockUserWithRefreshToken, grantType: 'invalidGrantType' };
+    const { statusCode, body } = await server.post('/auth/login')
+      .send(invalidGrantTypeUser);
+    expect(statusCode).toBe(400);
+    expect(body.message).toBe('child "body" fails because [child "grantType" fails because ["grantType" must be one of [password, refreshToken]]]');
+  });
+
+  it('should fail on obtain an accessToken with invalid refreshToken', async () => {
+    const invalidRefreshTokenUser = { ...mockUserWithRefreshToken, refreshToken: 'invalidRefreshToken' };
+    const { statusCode, body } = await server.post('/auth/login')
+      .send(invalidRefreshTokenUser);
+    expect(statusCode).toBe(400);
+    expect(body.message).toBe('Invalid refreshToken');
+  });
+
+  it('should fail on obtain an accessToken with no refreshToken', async () => {
+    const noRefreshTokenUser = { grantType: mockUserWithRefreshToken.grantType };
+    const { statusCode, body } = await server.post('/auth/login')
+      .send(noRefreshTokenUser);
+    expect(statusCode).toBe(400);
+    expect(body.message).toBe('child "body" fails because [child "refreshToken" fails because ["refreshToken" is required]]');
+  });
+
+  it('should obtain an accesToken successfully', async () => {
+    const { statusCode, body } = await server.post('/auth/login').send(mockUserWithRefreshToken);
+    expect(statusCode).toBe(201);
+    expect(body.success).toBeTruthy();
+    expect(body.message).toBe('Access token generated successfully');
+    expect(typeof body.accessToken).toBe('string');
+    expect(body.accessToken).not.toBe('');
+  });
+
   afterAll(async () => {
-    const userId = await User.findOne({ email: mockUser.email });
-    await User.remove({ _id: userId });
-    await Session.remove({ userId });
+    const user = await User.findOneAndRemove({ email: mockUser.email });
+    await Session.remove({ userId: user._id });
   });
 });
--- src/routes/auth/login.js
+++ src/routes/auth/login.js
@@ -11,32 +11,57 @@ const Session = mongoose.model('Session');
 
 router.post('/', validate({
   body: Joi.object().keys({
+    grantType: Joi.string()
+      .valid('password', 'refreshToken')
+      .required(),
     email: Joi.string()
       .email()
-      .required(),
+      .when('grantType', { is: 'password', then: Joi.required(), otherwise: Joi.forbidden() }),
     password: Joi.string()
-      .required(),
+      .when('grantType', { is: 'password', then: Joi.required(), otherwise: Joi.forbidden() }),
+    refreshToken: Joi.string()
+      .when('grantType', { is: 'refreshToken', then: Joi.required(), otherwise: Joi.forbidden() }),
   }),
 }), async (req, res, next) => {
-  const { email, password } = req.body;
+  const { grantType } = req.body;
 
-  try {
-    const user = await User.findOne({ email });
-    if (!user) throw genHttpError(400, 'User not found');
-    const isMatch = await user.comparePassword(password);
-    if (!isMatch) throw genHttpError(400, 'Wrong password');
-    const refreshToken = genRefreshToken();
-    const accessToken = await genAccessToken(user._id);
-    await new Session({ userId: user._id, refreshToken: sha256(refreshToken) }).save();
+  if (grantType === 'password') {
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) throw genHttpError(400, 'User not found');
+      const isMatch = await user.comparePassword(password);
+      if (!isMatch) throw genHttpError(400, 'Wrong password');
+      const refreshToken = genRefreshToken();
+      const accessToken = await genAccessToken(user._id);
+      await new Session({ userId: user._id, refreshToken: sha256(refreshToken) }).save();
 
-    res.status(201).json({
-      success: true,
-      message: 'Login session created successfully',
-      refreshToken,
-      accessToken,
-    });
-  } catch (err) {
-    next(err);
+      res.status(201).json({
+        success: true,
+        message: 'Login session created successfully',
+        refreshToken,
+        accessToken,
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  if (grantType === 'refreshToken') {
+    const { refreshToken } = req.body;
+    try {
+      const session = await Session.findOne({ refreshToken: sha256(refreshToken) });
+      if (!session) throw genHttpError(400, 'Invalid refreshToken');
+      const accessToken = await genAccessToken(session.userId);
+
+      res.status(201).json({
+        success: true,
+        message: 'Access token generated successfully',
+        accessToken,
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 });
 
